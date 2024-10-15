dd546724232bf2a47d7fa9bb3fb233c644d33e88
Kjell Naessens <kjell.naessens@student.kuleuven.be>
2024-10-15 10:50:20 +0200

    Update Led Pin

--- BesteArduinoProjectOoit/src/main.cpp
+++ BesteArduinoProjectOoit/src/main.cpp
@@ -1,6 +1,6 @@
 #include "Blink.h"
 
-Blink ledBlink(13);  // Create a Blink object for pin 13 (usually the built-in LED)
+Blink ledBlink(2);  // Create a Blink object for pin 13 (usually the built-in LED)
 
 void setup() {
   // No setup needed for this example since the Blink constructor handles pin setup
